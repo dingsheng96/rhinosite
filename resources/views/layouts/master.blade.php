9088f7e7cdacf52e258f542f2eec9781cde4b512
dingsheng96 <dingshengho@cspeed.com.my>
2021-08-16 22:14:46 +0800

    setup open graph for whatsapp

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>{{ config('app.name') }} @if (!empty($title)) {{ ' | ' . $title }} @endif</title>
+    <meta name="description" content="No.1 Contractor Platform in Malaysia Compare Price, Project Portfolios and Contractor's Specialization.">
+    <meta property="og:title" content="{{ config('app.name') }} @if (!empty($title)) {{ ' | ' . $title }} @endif" />
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:description" content="No.1 Contractor Platform in Malaysia Compare Price, Project Portfolios and Contractor's Specialization.">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="{{ asset('storage/logo_og.png?v=' . time()) }}">
+
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
