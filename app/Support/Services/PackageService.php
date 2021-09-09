43dc3bad0fffefcb5c76df75b17be3ffb7e88deb
dingsheng96 <dingshengho@cspeed.com.my>
2021-09-09 14:40:51 +0800

    package service update

--- app/Support/Services/PackageService.php
+++ app/Support/Services/PackageService.php
@@ -22,13 +22,14 @@ class PackageService extends BaseService
 
     public function storeDetails()
     {
-        $this->model->name          =   $this->request->get('name');
-        $this->model->description   =   $this->request->get('description');
-        $this->model->stock_type    =   $this->request->get('stock_type');
-        $this->model->quantity      =   $this->request->get('quantity');
-        $this->model->status        =   $this->request->get('status');
-        $this->model->recurring     =   $this->request->has('recurring');
-        $this->model->published     =   $this->request->has('published');
+        $this->model->name              =   $this->request->get('name');
+        $this->model->description       =   $this->request->get('description');
+        $this->model->stock_type        =   $this->request->get('stock_type');
+        $this->model->stock_quantity    =   $this->request->get('stock_quantity');
+        $this->model->quantity          =   $this->request->get('quantity');
+        $this->model->status            =   $this->request->get('status');
+        $this->model->recurring         =   $this->request->has('recurring');
+        $this->model->published         =   $this->request->has('published');
 
         if ($this->model->isDirty()) {
             $this->model->save();
